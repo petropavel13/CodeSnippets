f5fa3ad9c35d083bb82471f11bd54e73e4166455
petropavel <ivan.smolin@touchin.ru>
2018-12-13 17:35:01 +0300

    update install script

--- install_xcodesnippet.sh
+++ install_xcodesnippet.sh
@@ -4,6 +4,6 @@ cd /tmp
 
 git clone -b patch-1 https://github.com/mkhl/xcodesnippet /tmp/xcodesnippet
 
-cd xcodesnippet && rake build gem
+cd xcodesnippet && gem build xcodesnippet.gemspec
 
-gem install xcodesnippet-0.0.2.gem
+sudo gem install xcodesnippet-0.0.2.gem
